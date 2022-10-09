82991d30b4fc36a9156436a577fd4f374a953dd8
Francescatai <celine860702@gmail.com>
2022-10-09 19:37:37 +0800

    sum arithmetic sequence

--- discrete_mathematics_pratice.js
+++ discrete_mathematics_pratice.js
@@ -15,4 +15,36 @@ function ticket(age, price, weifare=false){
     }
 }
 
-console.log(ticket(15,200))
+// console.log(ticket(15,200))
+
+// sum_arithmetic_sequence
+// for loop
+function add(n){
+    let sum=0;
+    for(let i=1;i<=n;i++){
+        sum+=i;
+    }
+    console.log(sum);
+    return sum;
+}
+
+// add(10);
+
+// recursion
+function add2(n){
+    if(n==0){
+        return 0;
+    }else{
+        return n+add2(n-1);
+    }
+}
+
+// console.log(add2(10));
+
+// formula
+function add3(n){
+    console.log(((1+n)*n)/2)
+    return (((1+n)*n)/2);
+}
+
+add3(10);
